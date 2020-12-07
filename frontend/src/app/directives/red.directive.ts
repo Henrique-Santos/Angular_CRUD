c3185af25bfce38b476dd3ae2c26f8b3b7879278
Henrique Santos <henrique.144@outlook.com>
2020-12-07 17:47:53 -0300

    Criando diretivas

---
+++ frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appRed]'
+})
+export class RedDirective {
+
+  constructor(private elementRef: ElementRef) {
+    this.elementRef.nativeElement.style.color = '#E35E6B'
+  }
+
+}
