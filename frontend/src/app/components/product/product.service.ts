d781aa186a8958a8d6274c849b7ee88c18dedc89
Henrique Santos <henrique.144@outlook.com>
2020-12-08 17:39:06 -0300

    Customizando msg de erro

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 import { Product } from './product.model';
 
@@ -17,35 +18,42 @@ export class ProductService {
     private http: HttpClient
   ) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 300,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(baseUrl, product)
+      .pipe(map(obj => obj))
+      .pipe(catchError(e => this.errorHandler(e)))
   }
-
+  
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(baseUrl)
   }
-
+  
   readById(id: string | null): Observable<Product> {
     const url = `${baseUrl}/${id}`
     return this.http.get<Product>(url)
   }
-
+  
   update(product: Product): Observable<Product> {
     const url = `${baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
   }
-
+  
   delete(id: number | undefined): Observable<Product> {
     const url = `${baseUrl}/${id}`
     return this.http.delete<Product>(url)
   }
-
+  
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu em erro!', true)
+    return EMPTY
+  }
 }
